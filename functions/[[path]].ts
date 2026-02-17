6ea376bcb41105f63070c8efa5a92d4c938c4b1b
x-info <dmsadikins@gmail.com>
2026-02-17 21:12:33 +0700

    feat: Implement Cloudflare Pages Functions support for React Router, updating build scripts and adding necessary type definitions.

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,5 +1,15 @@
 import { createPagesFunctionHandler } from "@react-router/cloudflare";
-// @ts-ignore
-import * as build from "../build/server";
+
+declare module "react-router" {
+  export interface AppLoadContext {
+    cloudflare: {
+      env: Env;
+      ctx: ExecutionContext;
+    };
+  }
+}
+
+// @ts-ignore - virtual module resolved at build time
+const build = () => import("virtual:react-router/server-build");
 
 export const onRequest = createPagesFunctionHandler({ build });
---
+++ worker-configuration.d.ts
@@ -0,0 +1,5 @@
+// Generated Cloudflare environment bindings
+// Matches vars defined in wrangler.toml
+interface Env {
+  VENDURE_API_URL: string;
+}
